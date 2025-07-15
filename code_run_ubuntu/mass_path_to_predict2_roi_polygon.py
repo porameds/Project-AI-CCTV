7aabdaaa3542a714f179182dc65b5894a9f3b9d2
Porameds Sornwieng <poramedssw@kkumail.com>
2025-07-15 17:13:35 +0700

    refactor for easy read maybe hahahaha

--- code_run_ubuntu/mass_path_to_predict2_roi_polygon.py
+++ code_run_ubuntu/mass_path_to_predict2_roi_polygon.py
@@ -1,16 +1,13 @@
-
 import os
-import torch
 import cv2
+import time
+import torch
+import numpy as np
 import pandas as pd
+import psycopg2
 from datetime import datetime
 from ultralytics import YOLO
-import psycopg2
 from psycopg2.extras import execute_values
-import time
-import numpy as np
-import onnxruntime as ort
-
 
 """
 ROI_ALL ={
@@ -18,9 +15,6 @@ ROI_ALL ={
     "R2-07-12": [(399, 125), (525, 138), (511, 249), (388, 280)]
 }
 """
-
-#SELECTED_MACHINE = os.getenv("MACHINE_NAME","R2-07-11")
-
 
 CONFIG = {
     "model_path": "/app/oven_machine_model/weights/best.pt",
@@ -33,10 +27,7 @@ CONFIG = {
     "no_box_dirs": {
         "input1": "/app/test_oven_1/no_box"
     },
-    
 
-    # "roi_polygon": [  # พิกัด 4 จุดของกรอบ (ROI)
-  
     #"roi_polygons": {
         #"R2-07-11": [(308, 110), (456, 130), (445, 271), (301, 312)],
         #"R2-07-12": [(399, 125), (525, 138), (511, 249), (388, 280)]
@@ -45,9 +36,6 @@ CONFIG = {
         #"R2-07-11": [(175, 19), (273, 9), (406, 17), (406, 174), (218, 219), (177, 118)],
         "R2-07-11": [(308, 110), (456, 130), (445, 271), (301, 312)],
         "R2-07-12": [(399, 125), (525, 138), (511, 249), (388, 280)]
-
-
-       #ROI_ALL[SELECTED_MACHINE]
     },
 
     "confidence_threshold": 0.5,
@@ -58,10 +46,7 @@ CONFIG = {
     "show_frame_predict": False,
 }
 
-
-
 def insert_to_postgres(df):
-    
     try:
         values = [
             (
@@ -95,19 +80,18 @@ def insert_to_postgres(df):
     except Exception as e:
         print(f"[!] Failed to insert to DB: {e}")
 
-
 def is_box_inside_polygon(box, polygon):
     x1, y1, x2, y2 = box
     box_points = [
         (x1, y1), (x2, y1), (x2, y2), (x1, y2)
     ]
     polygon_np = np.array(polygon, dtype=np.int32)
-
     #for point in box_points:
       #  if cv2.pointPolygonTest(polygon_np, point, False) < 0:
            # return False
     #return True
     return all(cv2.pointPolygonTest(polygon_np, pt,False) >= 0 for pt in box_points)
+
 def gen_new_filename(machine_name, predict_time, detection_result, ext=".jpg"):
     safe_machine_name = machine_name
     # เปลี่ยน ":" กับ space เพื่อให้ชื่อไฟล์ปลอดภัย
@@ -119,25 +103,19 @@ def predict_images(input_dir, output_dir, model, name_tag, device="cpu"):
     os.makedirs(output_dir, exist_ok=True)
     no_box_dir = CONFIG["no_box_dirs"][name_tag]
     os.makedirs(no_box_dir, exist_ok=True)
-
     roi_polygons = CONFIG["roi_polygons"]
-
     image_files = sorted([
         f for f in os.listdir(input_dir)
         if f.lower().endswith(('.jpg', '.png'))
     ])
-
     results_list = []
-
     for img_file in image_files:
         img_path = os.path.join(input_dir, img_file)
         img = cv2.imread(img_path)
         original_img = img.copy()
         result = model.predict(img, device=device)[0]
-
         boxes = result.boxes
         machine_data = {mn: {"labels": set(), "confidences": [], "boxes": []} for mn in roi_polygons.keys()}
-
         if boxes and len(boxes.conf) > 0:
             for box, conf, cls in zip(boxes.xyxy.cpu().numpy(), boxes.conf.cpu().numpy(), boxes.cls.cpu().numpy().astype(int)):
                 if conf >= CONFIG["confidence_threshold"]:
@@ -146,22 +124,18 @@ def predict_images(input_dir, output_dir, model, name_tag, device="cpu"):
                     machine_name = get_box_machine_name(box_coords, roi_polygons)
                     if not machine_name:
                         continue
-                    
                     machine_data[machine_name]["labels"].add(label_name)
                     machine_data[machine_name]["confidences"].append(conf)
                     machine_data[machine_name]["boxes"].append(box_coords)
-
                     # วาดกล่องบนภาพ
                     label = f"{label_name} ({conf:.2f})"
                     cv2.rectangle(img, (box_coords[0], box_coords[1]), (box_coords[2], box_coords[3]), (0, 255, 0), 2)
                     cv2.putText(img, label, (box_coords[0], box_coords[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
         # วนลูปแยกเครื่อง
         for machine_name, data in machine_data.items():
             label_set = data["labels"]
             confidences = data["confidences"]
             boxes = data["boxes"]
-
             if label_set:
                 main_signal = 0
                 sub_signal = 0
@@ -173,12 +147,10 @@ def predict_images(input_dir, output_dir, model, name_tag, device="cpu"):
                         sub_signal = 3
                     elif "glove" in label_set or "black glove" in label_set:
                         sub_signal = 4
-
                 detection_result = ', '.join(sorted(label_set))
                 avg_conf = sum(confidences) / len(confidences) if confidences else 0.0
                 predict_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 new_filename = gen_new_filename(machine_name, predict_time, detection_result)
-
                 result_dict = {
                     "predict_time": predict_time,
                     "detection_result": detection_result,
@@ -189,21 +161,20 @@ def predict_images(input_dir, output_dir, model, name_tag, device="cpu"):
                     "machine_name": machine_name
                 }
                 results_list.append(result_dict)
-
                 # บันทึกภาพ no_box เฉพาะภาพที่เข้าเงื่อนไข
                 if sub_signal != 0 and main_signal != 0:
+
                     try:
                         cv2.imwrite(os.path.join(no_box_dir, new_filename), original_img)
                         print(f" Saved (no box): {new_filename}")
                     except Exception as e:
                         print(f"[!] Failed to save no-box image {new_filename}: {e}")
-
                 if CONFIG["save_image"]:
+
                     try:
                         cv2.imwrite(os.path.join(output_dir, new_filename), img)
                     except Exception as e:
                         print(f"[!] Failed to save image {new_filename}: {e}")
-
             else:
                 # กรณี no detection
                 predict_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -250,11 +221,6 @@ def predict_images(input_dir, output_dir, model, name_tag, device="cpu"):
         except Exception as e:
             print(f"[!] Failed to remove {img_path}: {e}")
 
-# def is_box_inside_polygon(box, polygon):
-#     x1, y1, x2, y2 = box
-#     box_points = [(x1, y1), (x2, y1), (x2, y2), (x1, y2)]
-#     polygon_np = np.array(polygon, dtype=np.int32)
-#     return all(cv2.pointPolygonTest(polygon_np, pt, False) >= 0 for pt in box_points)
 def get_box_machine_name(box, roi_polygons):
     for machine_name, polygon in roi_polygons.items():
         if is_box_inside_polygon(box, polygon):
@@ -262,11 +228,8 @@ def get_box_machine_name(box, roi_polygons):
     return None
 
 def run_path_to_predict_roi_polygon_oven():
-    # model = YOLO(CONFIG["model_path"])
-    # model.to('cuda')
     device = 0 if torch.cuda.is_available() else 'cpu'
     print(f"Using device: {'cuda' if device == 0 else 'cpu'}")
-
     model = YOLO(CONFIG["model_path"])
 
     try:
@@ -274,10 +237,8 @@ def run_path_to_predict_roi_polygon_oven():
             for name_tag, input_dir in CONFIG["input_dirs"].items():
                 output_dir = CONFIG["predict_dirs"][name_tag]
                 predict_images(input_dir, output_dir, model, name_tag)
-
             if CONFIG["show_frame_predict"]:
                 cv2.destroyAllWindows()
-
             time.sleep(5)
     except KeyboardInterrupt:
         print(" Stopped by user")
